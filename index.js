b05f2489322b48675c7b525e005e3c789733f8aa
Raraaa <raraasukaicecreamm@gmail.com>
2026-02-08 20:52:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const chalk = require("chalk")
 
 const oldLog = console.log
 console.log = (...args) => {
-  oldLog(chalk.cyanBright("#RaraaImupp"), ...args)
+  oldLog(chalk.cyanBright("<3 𝑹𝒂𝒓𝒂𝒂 𝑰𝒎𝒖𝒑𝒑"), ...args)
 }
 
 console.log("Baileysx2026 Loaded")
